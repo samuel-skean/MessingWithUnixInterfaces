a42cd7b47b45dcc5bf8401f9a2ff20132795bed2
Samuel Skean <sskean2@uic.edu>
2025-04-27 20:49:03 -0500

    Some notes.

--- responds_to_sighup_under_nohup/main.c
+++ responds_to_sighup_under_nohup/main.c
@@ -4,6 +4,24 @@
 #include <assert.h>
 #include <stdlib.h>
 
+// Notes:
+//
+// This program dies to SIGHUP when run with `nohup`.
+//
+// This includes (of course?) when it receives SIGHUP when a shell
+// propogates a SIGHUP it receives, as `bash` does by default. The SIGHUP
+// sent to the shell is often because an ssh session ended because of
+// connection issues or unceremonious termination.
+//
+// IMO, the `disown -h` built-in is just better when you can do it - it
+// prevents the propogation of the SIGHUP, while letting the process handle
+// SIGHUP as it sees fit.
+// Also, I think if `nohup` simply spawned a process running the program it
+// was tasked to run as a child, making it a grandchild of the shell, that
+// grandchild would be immune to the shell's propogation of SIGHUP (unless
+// of course `nohup` propogated it, which would be silly). That's something
+// to check!
+
 int logfile_fd;
 void sighub_sigaction_handler(int sig, siginfo_t *info, void *ucontext);
 	

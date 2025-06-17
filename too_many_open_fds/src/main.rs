71130e3340597a8a3a64c7d70e26c6c3a3df423a
Samuel Skean <sskean2@uic.edu>
2025-06-16 20:40:43 -0500

    files -> fds

--- too_many_open_fds/src/main.rs
+++ too_many_open_fds/src/main.rs
@@ -1,27 +1,28 @@
 use libc::{RLIMIT_NOFILE, getrlimit64, rlimit64};
 
-fn get_open_file_limits() -> rlimit64 {
-    let mut open_file_limits = rlimit64 {
+fn get_open_fds_limits() -> rlimit64 {
+    let mut open_fds_limits = rlimit64 {
         rlim_cur: 0,
         rlim_max: 0,
     };
 
-    let getrlimit64_ret = unsafe { getrlimit64(RLIMIT_NOFILE, &raw mut open_file_limits) };
+    let getrlimit64_ret = unsafe { getrlimit64(RLIMIT_NOFILE, &raw mut open_fds_limits) };
     assert_eq!(getrlimit64_ret, 0);
 
-    open_file_limits
+    open_fds_limits
 }
+
 fn main() {
-    let open_file_limits = get_open_file_limits();
-    println!("Here's the limits on the number of open files I'm allowed to have open:");
+    let open_fds_limits = get_open_fds_limits();
+    println!("Here's the limits on the number of open fdss I'm allowed to have open:");
 
     println!(
         "Soft limit (I can change this!): {}",
-        open_file_limits.rlim_cur
+        open_fds_limits.rlim_cur
     );
     println!(
         "Hard limit (I likely can't change this): {}",
-        open_file_limits.rlim_max
+        open_fds_limits.rlim_max
     );
 
     println!("Check back later for me to test these :)!");

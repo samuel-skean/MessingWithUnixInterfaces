4a071e5083acf8288a3c66458cf20b8ee7c9a93a
Samuel Skean <sskean2@uic.edu>
2025-06-16 21:08:34 -0500

    Use normal, non `*64` versions of the interface.
    See https://www.qnx.com/developers/docs/8.0/com.qnx.doc.neutrino.lib_ref/topic/summary.html#summary__LFS_class, specifically "Large File Support".

--- too_many_open_fds/src/main.rs
+++ too_many_open_fds/src/main.rs
@@ -1,13 +1,13 @@
-use libc::{RLIMIT_NOFILE, getrlimit64, rlimit64};
+use libc::{RLIMIT_NOFILE, getrlimit, rlimit};
 
-fn get_open_fds_limits() -> rlimit64 {
-    let mut open_fds_limits = rlimit64 {
+fn get_open_fds_limits() -> rlimit {
+    let mut open_fds_limits = rlimit {
         rlim_cur: 0,
         rlim_max: 0,
     };
 
-    let getrlimit64_ret = unsafe { getrlimit64(RLIMIT_NOFILE, &raw mut open_fds_limits) };
-    assert_eq!(getrlimit64_ret, 0);
+    let getrlimit_ret = unsafe { getrlimit(RLIMIT_NOFILE, &raw mut open_fds_limits) };
+    assert_eq!(getrlimit_ret, 0);
 
     open_fds_limits
 }

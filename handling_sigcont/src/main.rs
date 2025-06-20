f399f54749b1e0abfbbecd1de7f31b151be5eb89
Samuel Skean <sskean2@uic.edu>
2025-06-20 15:02:35 -0500

    Handling sigcont.

---
+++ handling_sigcont/src/main.rs
@@ -0,0 +1,29 @@
+use libc::*;
+
+// You can do it! It does resume your process after running your handler!
+
+extern "C" fn handler(signum: c_int, siginfo: *const siginfo_t, ucontext: *mut c_void) {
+    unsafe {
+        write(1, b"hi\n" as *const u8 as *const _, 3);
+    }
+}
+
+fn main() {
+    let sa_mask = unsafe { std::mem::zeroed() };
+
+    let sa = sigaction {
+        sa_sigaction: handler as usize,
+        sa_mask,
+        sa_flags: SA_SIGINFO,
+        sa_restorer: unsafe { std::mem::zeroed() },
+    };
+    unsafe {
+        sigaction(SIGCONT, &sa, std::ptr::null_mut());
+    }
+    loop {
+        unsafe {
+            write(1, b"bye\n" as *const u8 as *const _, 4);
+            std::thread::park_timeout(std::time::Duration::from_millis(500));
+        }
+    }
+}

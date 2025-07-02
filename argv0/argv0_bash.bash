3be9c5350557490f99b92fd148f08857ca6af92e
Samuel Skean <sskean2@uic.edu>
2025-07-02 10:55:32 -0500

    Typo fix.

--- argv0/argv0_bash.bash
+++ argv0/argv0_bash.bash
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # TODONOW: Print a nice message.
 echo "$0"
-echo $'On Linux, this prints the name used to invoke me when invoked with `bash <name>`. But when invoked directly, with `<name>`, it prints the concatenation of the entry in the PATH used to find me with the name passed on the command line as the zeroth argument. If no PATH entry was used to find me, it just prints the name passed on the command line as the zeroth argument.'
+echo $'On Linux, this prints the name used to invoke me when invoked with `bash <name>`. But when invoked directly, with `<name>`, it prints the concatenation of the entry in the PATH used to find it with the name passed on the command line as the zeroth argument. If no PATH entry was used to find me, it just prints the name passed on the command line as the zeroth argument.'
